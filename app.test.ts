8bab9b37314e94c433d3efd0ebc38823fccb75ea
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-16 17:27:20 -0300

    feat: put()

--- app.test.ts
+++ app.test.ts
@@ -15,4 +15,19 @@ describe('Chess', () => {
     expect(chess.getBoard()[0][0]).toBe('')
     expect(chess.getBoard()[0][1]).toBe('')
   })
+
+  test('Should put ok', () => {
+    const chess = makeBoard()
+
+    chess.put('p', '12')
+
+    expect(chess.getBoard()[1][2]).toEqual('p')
+  })
+
+  test('Should throw on put', () => {
+    const chess = makeBoard()
+
+    expect(() => chess.put('Q', '08')).toThrowError('Invalid Put')
+    expect(() => chess.put('Q', '08')).toThrowError('Invalid Put')
+  })
 })
--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
 type Board = string[][]
+type Square = string
+type Piece = string
 
 export class Chess {
   private readonly board: Board
+  private readonly limit: number
 
   constructor () {
     this.board = []
+    this.limit = 8
 
-    for (let y = 0; y < 8; y++) {
+    for (let y = 0; y < this.limit; y++) {
       this.board[y] = []
 
-      for (let x = 0; x < 8; x++) {
+      for (let x = 0; x < this.limit; x++) {
         this.board[y][x] = ''
       }
     }
@@ -17,5 +21,21 @@ export class Chess {
 
   public getBoard (): Board {
     return this.board
+  }
+
+  public put (piece: Piece, square: Square): void {
+    const [y, x] = this.getNumericPosition(square)
+
+    if (y >= this.limit || x >= this.limit) {
+      throw new Error('Invalid Put')
+    }
+
+    this.board[y][x] = piece
+  }
+
+  private getNumericPosition (square: Square): number[] {
+    const [y, x] = square.split('')
+
+    return [parseInt(y), parseInt(x)]
   }
 }
