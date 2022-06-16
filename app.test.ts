beebc0c151c72047b25c7360cda81c2131edea2e
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-16 12:04:13 -0300

    feat: return board on get

--- app.test.ts
+++ app.test.ts
@@ -3,6 +3,14 @@ import { Chess } from './app'
 describe('Chess', () => {
   test('Should construct ok', () => {
     const makeGame = new Chess()
+
     expect(makeGame).toBeTruthy()
+    expect(makeGame.board).toBeTruthy()
+  })
+
+  test('Should return board on get', () => {
+    const makeGame = new Chess()
+
+    expect(makeGame.getBoard()).toBeTruthy()
   })
 })
--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
-export class Chess {}
+type Board = number[]
+
+export class Chess {
+  private readonly size: number
+  public readonly board: Board
+
+  constructor () {
+    const dimensions = 8
+
+    this.size = dimensions * dimensions
+    this.board = []
+
+    for (let index = 0; index < this.size; index++) {
+      this.board[index] = 0
+    }
+  }
+
+  public getBoard (): Board {
+    return this.board
+  }
+}
