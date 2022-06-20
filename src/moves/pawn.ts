48f01ec58e0af131044921a823fcaa7aa847a47f
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-20 15:24:37 -0300

    feat: pawn moves

---
+++ src/moves/pawn.ts
@@ -0,0 +1,11 @@
+import { Position } from '../utils'
+import { isYAxisMove, isXAxisMove, isDiagonalMove, isMoreThanSingleSquare } from './validations'
+
+export function canPawnMove (current: Position, next: Position): boolean {
+  if (!isMoreThanSingleSquare(current, next)) return false
+  if (isYAxisMove(current, next)) return true
+  if (isXAxisMove(current, next)) return true
+  if (isDiagonalMove(current, next)) return true
+
+  return false
+}
