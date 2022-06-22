6b4392fdda535767ffe75a3ac23c7d12766175c1
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-21 23:28:46 -0300

    fix: pawn moves

--- src/moves/pawn.test.ts
+++ src/moves/pawn.test.ts
@@ -4,12 +4,9 @@ describe('Pawn', () => {
   describe('canPawnMove()', () => {
     test('to return properly', () => {
       expect(canPawnMove('d4', 'd5')).toBe(true)
-      expect(canPawnMove('d4', 'e4')).toBe(true)
       expect(canPawnMove('d4', 'e5')).toBe(true)
-      expect(canPawnMove('d4', 'd6')).toBe(false)
-      expect(canPawnMove('d4', 'f4')).toBe(false)
+      expect(canPawnMove('d4', 'c5')).toBe(true)
       expect(canPawnMove('d4', 'f6')).toBe(false)
-      expect(canPawnMove('d4', 'e2')).toBe(false)
     })
   })
 })
