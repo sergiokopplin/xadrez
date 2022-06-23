1976199e2cdeade2d8152f236d61654485a28a56
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-22 21:40:06 -0300

    feat: populate squares

--- src/board/board.test.ts
+++ src/board/board.test.ts
@@ -1,16 +1,14 @@
 import { Board } from './board'
 
 describe('Board', () => {
-  test('Should Create a board with correct size', () => {
-    const sut = new Board()
-    expect(sut.board.length).toEqual(8)
-    expect(sut.board[0].length).toEqual(8)
+  test('Should Create a board', () => {
+    expect(new Board()).toBeTruthy()
   })
 
-  test('Should Create a board and populate empty spaces Correctly', () => {
+  test('Should Create a board and populate squares correctly', () => {
     const emptyRow = new Array(8).fill(null)
-    const board = new Array(8).fill(emptyRow)
+    const sut = new Board()
 
-    expect(new Board().board).toEqual(board)
+    expect(sut.board).toEqual([[], [], emptyRow, emptyRow, emptyRow, emptyRow, [], []])
   })
 })
--- src/board/board.ts
+++ src/board/board.ts
@@ -6,6 +6,12 @@ export class Board {
   }
 
   private populateBoard () {
-    return new Array(8).fill(new Array(8).fill(null))
+    const board = new Array(8).fill([])
+    const emptyRow = new Array(8).fill(null)
+    const emptyRows = [2, 3, 4, 5]
+
+    emptyRows.forEach(row => { board[row] = emptyRow })
+
+    return board
   }
 }
