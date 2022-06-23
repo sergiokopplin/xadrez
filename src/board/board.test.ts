5eb236fbba25d086da56be57435c3489c6125f19
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-23 20:06:20 -0300

    chore: refactor board tests

--- src/board/board.test.ts
+++ src/board/board.test.ts
@@ -1,12 +1,14 @@
 import { Board } from './board'
 
 describe('Board', () => {
-  it('ensure board creation', () => {
-    expect(new Board()).toBeTruthy()
-  })
+  describe('constructor', () => {
+    it('ensure board creation', () => {
+      expect(new Board()).toBeTruthy()
+    })
 
-  it('ensure board create and populate squares correctly', () => {
-    const sut = new Board()
-    expect(sut.board).toMatchSnapshot()
+    it('ensure board create and populate squares correctly', () => {
+      const sut = new Board()
+      expect(sut.board).toMatchSnapshot()
+    })
   })
 })
