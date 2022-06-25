5c35505efbef627e0398306e6ee98f727d059e8c
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-24 22:17:52 -0300

    feat: bishop hasBlockPiece validations

--- src/pieces/bishop.ts
+++ src/pieces/bishop.ts
@@ -1,14 +1,30 @@
-import { Board } from '../board/board'
+import { Square } from '../board/board'
+import { getPositionAxis } from '../utils'
 import { Piece } from './piece'
 import { isDiagonalMove, isMoreThanSingleSquare } from './validations'
 
 export class Bishop implements Piece {
-  hasBlockPiece (current: string, next: string, board: Board): boolean {
+  hasBlockPiece (current: string, next: string, board: Square[]): boolean {
+    const [cy, cx] = getPositionAxis(current)
+    const [ny, nx] = getPositionAxis(next)
+
     if (!isMoreThanSingleSquare(current, next)) {
       return false
     }
 
-    return true
+    if (cy < ny) {
+      for (let index = cy + 1; index < ny; index++) {
+        if (board[index][index]) {
+          return false
+        }
+      }
+    } else if (cy > ny) {
+      for (let index = cy - 1; index > ny; index--) {
+        if (board[index][index]) {
+          return false
+        }
+      }
+    }
   }
 
   move (current: string, next: string): boolean {
