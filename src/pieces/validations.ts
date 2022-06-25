4bf9f60631e335e92d6fcb0834f1ff26747ee57d
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-25 13:33:11 -0300

    fix: remove sonar cloud smells

--- src/pieces/validations.ts
+++ src/pieces/validations.ts
@@ -4,15 +4,15 @@ import { Square } from '../board/board'
 import { fixArrayIndex, getPositionAxis, Position } from '../utils'
 
 export function isYAxisMove (current: Position, next: Position): boolean {
-  const [cy, cx] = getPositionAxis(current)
-  const [ny, nx] = getPositionAxis(next)
+  const [cy] = getPositionAxis(current)
+  const [ny] = getPositionAxis(next)
 
   return cy === ny
 }
 
 export function isXAxisMove (current: Position, next: Position): boolean {
-  const [cy, cx] = getPositionAxis(current)
-  const [ny, nx] = getPositionAxis(next)
+  const [, cx] = getPositionAxis(current)
+  const [, nx] = getPositionAxis(next)
 
   return cx === nx
 }
@@ -47,8 +47,8 @@ export function isMoreThanSingleSquare (current: Position, next: Position): bool
 }
 
 export function hasBlockPieceOnDiagonal (current: string, next: string, board: Square[]): boolean {
-  const [cy, cx] = getPositionAxis(current)
-  const [ny, nx] = getPositionAxis(next)
+  const [cy] = getPositionAxis(current)
+  const [ny] = getPositionAxis(next)
   const [CY, NY] = fixArrayIndex(cy, ny)
 
   if (CY < NY) {
@@ -73,8 +73,8 @@ export function hasBlockPieceOnDiagonal (current: string, next: string, board: S
 export function hasBlockPieceOnYAxis (current: string, next: string, board: Square[]): boolean {
   const [cy, cx] = getPositionAxis(current)
   const [ny, nx] = getPositionAxis(next)
-  const [CY, CX] = fixArrayIndex(cy, cx)
-  const [NY, NX] = fixArrayIndex(ny, nx)
+  const [, CX] = fixArrayIndex(cy, cx)
+  const [, NX] = fixArrayIndex(ny, nx)
 
   if (CX < NX) {
     for (let index = CX + 1; index < NX; index++) {
@@ -98,8 +98,8 @@ export function hasBlockPieceOnYAxis (current: string, next: string, board: Squa
 export function hasBlockPieceOnXAxis (current: string, next: string, board: Square[]): boolean {
   const [cy, cx] = getPositionAxis(current)
   const [ny, nx] = getPositionAxis(next)
-  const [CY, CX] = fixArrayIndex(cy, cx)
-  const [NY, NX] = fixArrayIndex(ny, nx)
+  const [CY] = fixArrayIndex(cy, cx)
+  const [NY] = fixArrayIndex(ny, nx)
 
   if (CY < NY) {
     for (let index = CY + 1; index < NY; index++) {
