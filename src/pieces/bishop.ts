e63e9e1dfce6febc7149ad27a85b357b96473e64
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-26 17:10:49 -0300

    chore: removes unused code

--- src/pieces/bishop.ts
+++ src/pieces/bishop.ts
@@ -1,10 +1,6 @@
-import { Board, Square } from '../board/board'
+import { Square } from '../board/board'
 import { Piece } from './piece'
 import { hasBlockPieceOnDiagonal, isDiagonalMove } from './validations'
-
-export const makeBoard = (): Board => {
-  return new Board()
-}
 
 export class Bishop implements Piece {
   move (current: string, next: string, board: Square[]): boolean {
