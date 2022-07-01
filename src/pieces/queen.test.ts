fdfb857e8c276030c396a4b684ea4c1ced98013a
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-30 21:14:20 -0300

    chore: refactor queen specs

--- src/pieces/queen.test.ts
+++ src/pieces/queen.test.ts
@@ -11,12 +11,17 @@ export const makeBoard = (): Board => new Board()
 
 describe('Queen', () => {
   describe('move()', () => {
-    test('to return properly', () => {
+    test('to return properly with valid moves', () => {
       const { sut } = makeSut()
 
       expect(sut.move('c4', 'c8', makeBoard().board)).toBe(true)
       expect(sut.move('c4', 'a4', makeBoard().board)).toBe(true)
       expect(sut.move('c4', 'g8', makeBoard().board)).toBe(true)
+    })
+
+    test('to return properly with invalid moves', () => {
+      const { sut } = makeSut()
+
       expect(sut.move('c4', 'a1', makeBoard().board)).toBe(false)
     })
   })
